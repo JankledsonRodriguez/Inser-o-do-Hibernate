af8b0e6e4011ec49b76d70f624c3f0a3fb88d3f7
JankledsonRodriguez <jankledson.rodrigues@gmail.com>
2026-06-24 11:45:26 -0300

    Inserção do Hibernate Apresentado em Sala de Aula

---
+++ src/main/java/com/example/primeiroAppSpring/controller/UsuarioController.java
@@ -0,0 +1,85 @@
+package com.example.primeiroAppSpring.controller;
+
+import com.example.primeiroAppSpring.model.UsuarioForm;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class UsuarioController {
+    @GetMapping("/cadastro")
+    public String exibirCadastro(Model model){
+        model.addAttribute("usuarioForm",new UsuarioForm());
+
+        model.addAttribute("tituloPagina","Cadastro");
+
+        model.addAttribute("subTituloPagina","Sistema de Gerenciamento de Estoque da Cozinha");
+        return  "cadastro";
+    }
+
+    @PostMapping("/cadastro")
+    public String processarCadastro(@ModelAttribute UsuarioForm form, Model model){
+        if (!form.getSenha().equals(form.getConfirmarSenha())){
+            model.addAttribute("erro","ERRO, as senhas não conferem");
+            return "cadastro";
+        }
+        IO.println("Usuário criado com sucesso");
+        IO.println(form.getNome()+" "+form.getEmail());
+
+        return "redirect:/login";
+    }
+
+
+    @GetMapping("/login")
+    public String exibirLogin(Model model){
+        model.addAttribute("usuarioForm",new UsuarioForm());
+
+        model.addAttribute("tituloPagina","Login");
+
+        model.addAttribute("subTituloPagina","Sistema de Gerenciamento de Estoque da Cozinha");
+        return  "login";
+    }
+
+    @PostMapping("/login")
+    public String processarLogin(@ModelAttribute UsuarioForm form, Model model){
+        if (form.getEmail().endsWith ("@df.senac.br")){
+            return "redirect:/";
+        }
+        model.addAttribute("erro", "E-mail ou senha inválidos");
+        IO.println(form.getNome()+" "+form.getEmail());
+
+        return "login";
+    }
+
+
+    @GetMapping("/alterar-senha")
+    public String exibirAlterarSenha(Model model){
+        model.addAttribute("usuarioForm",new UsuarioForm());
+
+        model.addAttribute("tituloPagina","Alterar Senha");
+
+        model.addAttribute("subTituloPagina","Informe a sua Senha");
+        return  "alterarSenha";
+    }
+
+
+
+    @PostMapping("/alterar-senha")
+    public String processarAlterarSenha(@ModelAttribute UsuarioForm form, Model model){
+        if (!form.getSenha().equals(form.getConfirmarSenha())){
+            model.addAttribute("erro","ERRO, as senhas não conferem");
+            return "alterar-senha";
+        }
+
+        model.addAttribute("SUCESSO!", " Senha alterada com SUCESSO!");
+
+        return "redirect:/login";
+    }
+
+}
+
+
